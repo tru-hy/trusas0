e1cb9e48b416ea322c7ad921de644ac5d6fc2c86
Jami Pekkanen <jami.pekkanen@helsinki.fi>
2012-10-29 23:43:11 +0200

    Added a pipeline script for Nexus

--- pipelines/tru.sh
+++ pipelines/tru.sh
@@ -2,9 +2,11 @@
 set -e
 
 DST=$1
+NEXUS_ADDR="00:A0:96:2F:A8:A6"
 
 mkdir $DST
 
 ./sensors.sh $DST/sensors.repr &
 ./location.sh $DST/location.repr &
+./nexus.sh $NEXUS_ADDR $DST/physiology.repr &
 ../gstreamer/uvch264record.py -v /dev/video0 $DST/video.mkv
