16883bb96249a5e6875a64cab38cc9b8c65dc8d6
Jami Pekkanen <jami.pekkanen@helsinki.fi>
2012-11-03 00:57:36 +0200

    Made transform.py do even less

--- transform.py
+++ transform.py
@@ -8,33 +8,29 @@ NOTE: This allows execution of arbitrary python code, so use with care!
 :todo: Seems very slow
 """
 import argh
-from packing import ReprPack, ReprUnpack
+from packing import default_packer, default_unpacker
 import sys
 import logging; log = logging.getLogger(__name__)
 
-def transformer(input, output, transforms):
-	for header, data in input:
-		for key, func in transforms.iteritems():
-			if key not in data: continue
-			try:
-				data[key] = func(data[key])
-			except Exception, e:
-				log.error("Transformation exception: %s"%e)
-		output.send(data, header=header)
+def _generate_function(source):
+	# There is a "nicer way" for this using types.FunctionType,
+	# but that would add about 20 lines with no real benefit
+	exec("def func(d, header): "+source)
+	return func
 
 @argh.plain_signature
 @argh.arg('transformations', type=str, nargs='+')
 def main(transformations):
-	transfs = {}
-	for trans_spec in transformations:
-		name, func = trans_spec.split("=", 1)
-		func = eval(func)
-		transfs[name] = func
+	global d # Not passing d as a parameter 
+	funcs = map(_generate_function, transformations)
 	
-	input = ReprUnpack(sys.stdin)
-	output = ReprPack(sys.stdout)
-	transformer(input, output, transfs)
-
+	input = default_unpacker()
+	output = default_packer()
+	for header, d in input:
+		for func in funcs:
+			d = func(d, header)
+		output.send(d, header=header)
+	
 if __name__ == '__main__':
 	parser = argh.ArghParser()
 	parser.add_commands([argh.alias('')(main)])
