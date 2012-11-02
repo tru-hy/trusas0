5ef3a6ef60b185d78bd5f9b45eb0eac6fec18f40
Jami Pekkanen <jami.pekkanen@helsinki.fi>
2012-11-03 00:57:07 +0200

    Added a stupid timestamper service

---
+++ timestamper.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+"""
+A stupid service that just repeteadly sends the current time.
+Mostly useful for testing or showing the current time in an
+overkill (albeit more elegant ;) way.
+"""
+import time, sys, argh
+from packing import default_packer
+
+@argh.command
+def main(interval=0.1):
+	output = default_packer()
+	while True:
+		output.send({'time': time.time()})
+		time.sleep(interval)
+
+if __name__ == '__main__':
+	parser = argh.ArghParser()
+	parser.add_commands([argh.alias('')(main)])
+	parser.dispatch(argv=['']+sys.argv[1:])
+	
