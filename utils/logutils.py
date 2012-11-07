c87601d663a4d6f6bcd9a91c119a58c5019cf70a
Jami Pekkanen <jami.pekkanen@helsinki.fi>
2012-11-07 17:41:16 +0200

    Added a log watcher class

--- utils/logutils.py
+++ utils/logutils.py
@@ -3,6 +3,7 @@ import json
 import time
 import inspect
 import sys
+import os
 
 class RobustJsonEncoder(json.JSONEncoder):
 	def default(self, obj):
@@ -25,9 +26,7 @@ class GmtJsonFormatter(logging.Formatter):
 
 	def format(self, record):
 		if record.exc_info:
-			print record.exc_info
 			record.exc_text = self.formatException(record.exc_info)
-			print record.exc_text
 		elif record.args:
 			record.exc_text = self.formatException(record.exc_info)
 
@@ -91,3 +90,162 @@ def get_logger(do_configure_logger=True):
 
 log = get_logger() # Eat the own poison
 
+import errno
+import select
+from StringIO import StringIO
+from itertools import chain
+
+def _stringio_getline(s):
+	line_delim = '\n' # TODO: There may be a more portable way for this
+	for i, buf in enumerate(s.buflist):
+		if line_delim in buf:
+			break
+	else:
+		return None
+
+	last, not_ours = s.buflist[i].split(line_delim, 1)
+	line = "".join(chain(s.buflist[:i], [last]))
+	s.buflist = [not_ours] + s.buflist[i+1:]
+	return line
+	
+
+class PollingFileWatcher(object):
+	"""
+	There's a python module for inotify which would be definitely
+	nicer. 
+	"""
+	def __init__(self, poll_interval=0.1):
+		self._paths = []
+		self._files = []
+		self._buffers = {}
+		self.poll_interval = poll_interval
+
+	def add_path(self, path):
+		if os.path.isdir(path):
+			raise TypeError("Can't watch directory path %s"%str(path))
+		
+		self._paths.append(path)
+		self.__open_file(path)
+		
+	def on_exception(self, path, exc_info):
+		"""
+		Handler for exceptions as we don't want to raise
+		from the next-method. Overwrite/hook on this for
+		custom handling
+		"""
+		log.exception("Watching file %s failed."%path)
+
+	def __open_file(self, path):
+		try:
+			fobj = open(path, 'r')
+			self._files.append(fobj)
+			self._buffers[path] = StringIO()
+		except IOError, e:
+			pass
+			#if e.errno != errno.ENOENT:
+			#	raise
+			# We'll watch if this gets created
+
+			
+
+	def __iter__(self): return self
+
+	def __probe_created_files(self):
+		existing = [f.name for f in self._files]
+		missing = (p for p in self._paths if p not in existing)
+		for path in missing:
+			try:
+				self.__open_file(path)
+			except:
+				self.on_exception(self, path, sys.exc_info())
+
+	def __read_to_buf(self, f):
+		try:
+			data = f.read()
+			# Here's the fun part. Select considers
+			# EOF'd files to be 'ready for reading',
+			# so the select is in most cases useless
+			# for us.
+			if not data:
+				# Don't clutter the buffer with empty strings
+				return
+			path = f.name
+			self._buffers[path].write(data)
+		except:
+			self.on_exception(path, sys.exc_info())
+
+	def next(self):
+		while True:
+			# First return any data we may have in the
+			# buffers
+			for path, buf in self._buffers.iteritems():
+				line =  _stringio_getline(buf)
+				if line is not None:
+					return path, line
+			
+			# Check if there's files to be watched
+			self.__probe_created_files()
+			# The sleep is due to not-so-suiteble for us
+			# behavior of select, see __read_to_buf.
+			time.sleep(self.poll_interval) 
+			ready, ign, ign = select.select(
+					self._files, [], [],
+					self.poll_interval)
+			
+			for f in ready:
+				self.__read_to_buf(f)
+				
+
+# TODO: This may be replaced by a nicer inotify-implementation
+#	later
+FileWatcher = PollingFileWatcher
+			
+class LogWatcher(FileWatcher):
+	"""
+	Iterates over given log files
+
+	>>> from threading import Thread
+	>>> from tempfile import mkdtemp
+	>>> from os import path
+	>>> tmpdir = mkdtemp(prefix="logutils_test")
+	>>> log1 = logging.Logger("foo")
+	>>> log1.addHandler(logging.FileHandler(path.join(tmpdir, "log1.log")))
+	>>> log1.handlers[-1].setFormatter(log_formatter)
+	>>> log1.error("message1")
+	>>>
+	>>> watcher = LogWatcher()
+	>>> watcher.add_path(log1.handlers[-1].baseFilename)
+	>>> logpath, record = watcher.next()
+	>>> str(path.basename(logpath))
+	'log1.log'
+	>>> str(record['msg'])
+	'message1'
+	>>>
+	>>> watcher.add_path(path.join(tmpdir, "log2.log"))
+	>>> log2 = logging.Logger("bar")
+	>>> log2.addHandler(logging.FileHandler(path.join(tmpdir, "log2.log")))
+	>>> log2.handlers[-1].setFormatter(log_formatter)
+	>>> log2.error("message2")
+	>>> logpath, record = watcher.next()
+	>>> str(path.basename(logpath))
+	'log2.log'
+	>>> str(record['msg']) 
+	'message2'
+	
+	"""
+	def __init__(self, formatter=log_formatter, **kwargs):
+		super(LogWatcher, self).__init__(**kwargs)
+		self.formatter = formatter
+	
+	def next(self):
+		path, line = FileWatcher.next(self)
+		while True:
+			try:
+				return path, self.formatter.parse(line)
+			except:
+				self.on_exception(path, sys.exc_info())
+
+
+if __name__ == '__main__':
+	import doctest
+	doctest.testmod()
