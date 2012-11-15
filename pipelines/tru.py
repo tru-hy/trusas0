5b44249ac0412d6ab6725df5f7754b25f3fc0ee0
Jami Pekkanen <jami.pekkanen@helsinki.fi>
2012-11-15 19:35:40 +0200

    Made less annoying tru pipeline startup possible

--- pipelines/tru.py
+++ pipelines/tru.py
@@ -2,20 +2,23 @@
 
 from os import path
 import sys
-# TODO: A hack
-sys.path.append(path.realpath("/home/jampekka/pro"))
 
 from trusas0.script import sh
 from trusas0 import ROOT
 from trusas0.service import ServiceSpec
 from trusas0.ui import run_ui
 import logging
+import os
 
 
 NEXUS_ADDR = "00:A0:96:2F:A8:A6"
 VIDEO_DEVICE = "/dev/video0"
 UDP_PREVIEW_PORT=5000
-BASE_SESSION_DIR=sh("zenity --title \\\"Base directory for sessions.\\\" --file-selection --directory").std_out.strip()
+
+BASE_SESSION_DIR=os.getenv("TRUSAS_BASE_DIR")
+
+if BASE_SESSION_DIR is None:
+	BASE_SESSION_DIR=sh("zenity --title \\\"Base directory for sessions.\\\" --file-selection --directory").std_out.strip()
 
 mypath=path.dirname(path.realpath(__file__))
 
