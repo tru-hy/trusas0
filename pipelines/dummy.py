ffcdb800a7e68ac33f8efe6d4914f2abb0378580
Jami Pekkanen <jami.pekkanen@helsinki.fi>
2013-10-15 16:29:48 +0300

    Fixed dummy pipeline to work with the new API

--- pipelines/dummy.py
+++ pipelines/dummy.py
@@ -1,8 +1,10 @@
 from trusas0 import ROOT
 from trusas0.service import ServiceSpec
-from trusas0.ui import SessionUi
+from trusas0.ui import run_ui
 
 s = ServiceSpec()
 s['dummy'] = ROOT+'/dummy_service.py'
 
-SessionUi(s, '/tmp/sessions').run()
+content = """<h3>Just a dummy session</h3>
+	<p>Nothing to see here.</p>"""
+run_ui(spec=s, base_dir='/tmp/sessions', content=content)
